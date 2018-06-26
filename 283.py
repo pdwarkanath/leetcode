e2f4d03f7a97a6865848a4da298e423b3033d0a5
pdwarkanath <dwarkanath.prabhu@gmail.com>
2018-06-26 03:58:11 -0500

    Created 283,py

---
+++ 283.py
@@ -0,0 +1,27 @@
+"""
+Name: Move Zeroes
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+
+"""
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        
+        for i in nums:
+            if i == 0:
+                nums.append(nums.pop(nums.index(i)))
+            else:
+                continue
